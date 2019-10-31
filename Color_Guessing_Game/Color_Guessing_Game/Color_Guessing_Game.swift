863e443499953ca9ac555c58feb22851df464fb2
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-10-31 14:55:15 -0400

    Buttons/Label Created

---
+++ Color_Guessing_Game/Color_Guessing_Game/Color_Guessing_Game.swift
@@ -0,0 +1,15 @@
+//
+//  Color_Guessing_Game.swift
+//  Color_Guessing_Game
+//
+//  Created by Juan Ceballos on 10/30/19.
+//  Copyright © 2019 Juan Ceballos. All rights reserved.
+//
+
+import Foundation
+
+struct Color_Guessing_Game  {
+    var highScore: Int
+    var score: Int
+    
+}
